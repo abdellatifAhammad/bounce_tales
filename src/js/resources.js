d957f99fded4af9be59b3e2c872450917ee6e238
AbdellatifAhammad <abdellatif.ahammad@gmail.com>
2023-10-12 07:31:58 +0100

    build: build the app to test mobile features

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,79 +2,79 @@ var resources = [
 
     /* Graphics.
     * @example
-    * { name: "example", type:"image", src: "../data/img/example.png" },
+    * { name: "example", type:"image", src: "/bounce_tales/data/img/example.png" },
     */
-    { name: "tileset", type: "image", src: "../data/img/tileset.png" },
-    { name: "background", type: "image", src: "../data/img/background.png" },
-    { name: "clouds", type: "image", src: "../data/img/clouds.png" },
-    { name: "tree", type: "image", src: "../data/img/tree.png" },
-    { name: "side_tree", type: "image", src: "../data/img/side_tree.png" },
-    { name: "improved_tileset", type: "image", src: "../data/img/improved_tileset.png" },
-    { name: "blocks", type: "image", src: "../data/img/blocks.png" },
-    { name: "bouncing_tales_map1", type: "image", src: "../data/img/bouncing_tales_map1.png" },
-    { name: "sky_trees", type: "image", src: "../data/img/sky_trees.png" },
-    { name: "egg", type: "image", src: "../data/img/egg.png" },
-    { name: "barrier", type: "image", src: "../data/img/barrier.png" },
-    { name: "title_screen", type: "image", src: "../data/img/title_screen.png" },
+    { name: "tileset", type: "image", src: "/bounce_tales/data/img/tileset.png" },
+    { name: "background", type: "image", src: "/bounce_tales/data/img/background.png" },
+    { name: "clouds", type: "image", src: "/bounce_tales/data/img/clouds.png" },
+    { name: "tree", type: "image", src: "/bounce_tales/data/img/tree.png" },
+    { name: "side_tree", type: "image", src: "/bounce_tales/data/img/side_tree.png" },
+    { name: "improved_tileset", type: "image", src: "/bounce_tales/data/img/improved_tileset.png" },
+    { name: "blocks", type: "image", src: "/bounce_tales/data/img/blocks.png" },
+    { name: "bouncing_tales_map1", type: "image", src: "/bounce_tales/data/img/bouncing_tales_map1.png" },
+    { name: "sky_trees", type: "image", src: "/bounce_tales/data/img/sky_trees.png" },
+    { name: "egg", type: "image", src: "/bounce_tales/data/img/egg.png" },
+    { name: "barrier", type: "image", src: "/bounce_tales/data/img/barrier.png" },
+    { name: "title_screen", type: "image", src: "/bounce_tales/data/img/title_screen.png" },
 
 
 
     /* Maps.
      * @example
-     * { name: "example01", type: "tmx", src: "../data/map/example01.tmx" },
-     * { name: "example01", type: "tmx", src: "../data/map/example01.json" },
+     * { name: "example01", type: "tmx", src: "/bounce_tales/data/map/example01.tmx" },
+     * { name: "example01", type: "tmx", src: "/bounce_tales/data/map/example01.json" },
      */
-    { name: "level1_map", type: "tmx", src: "../data/map/level1_map.tmx" },
-    { name: "level2_map", type: "tmx", src: "../data/map/level1_map.tmx" },
+    { name: "level1_map", type: "tmx", src: "/bounce_tales/data/map/level1_map.tmx" },
+    { name: "level2_map", type: "tmx", src: "/bounce_tales/data/map/level1_map.tmx" },
 
 
     /* Tilesets.
      * @example
-     * { name: "example01", type: "tsx", src: "../data/map/example01.tsx" },
-     * { name: "example01", type: "tsx", src: "../data/map/example01.json" },
+     * { name: "example01", type: "tsx", src: "/bounce_tales/data/map/example01.tsx" },
+     * { name: "example01", type: "tsx", src: "/bounce_tales/data/map/example01.json" },
      */
-    { name: "tileset", type: "tsx", src: "../data/map/tileset.tsx" },
-    { name: "side_tree", type: "tsx", src: "../data/map/side_tree.tsx" },
-    { name: "tree", type: "tsx", src: "../data/map/tree.tsx" },
-    { name: "improved_tileset", type: "tsx", src: "../data/map/improved_tileset.tsx" },
-    { name: "blocks", type: "tsx", src: "../data/map/blocks.tsx" },
-    { name: "sky", type: "tsx", src: "../data/map/sky.tsx" },
-    { name: "bouncing_tales_map1", type: "tsx", src: "../data/map/bouncing_tales_map1.tsx" },
+    { name: "tileset", type: "tsx", src: "/bounce_tales/data/map/tileset.tsx" },
+    { name: "side_tree", type: "tsx", src: "/bounce_tales/data/map/side_tree.tsx" },
+    { name: "tree", type: "tsx", src: "/bounce_tales/data/map/tree.tsx" },
+    { name: "improved_tileset", type: "tsx", src: "/bounce_tales/data/map/improved_tileset.tsx" },
+    { name: "blocks", type: "tsx", src: "/bounce_tales/data/map/blocks.tsx" },
+    { name: "sky", type: "tsx", src: "/bounce_tales/data/map/sky.tsx" },
+    { name: "bouncing_tales_map1", type: "tsx", src: "/bounce_tales/data/map/bouncing_tales_map1.tsx" },
 
 
 
     /* Atlases
     * @example
-    * { name: "example_tps", type: "json", src: "../data/img/example_tps.json" },
+    * { name: "example_tps", type: "json", src: "/bounce_tales/data/img/example_tps.json" },
     */
     // texturePacker
-    { name: "bounce", type: "image", src: "../data/img/bounce.png" },
-    { name: "bounce", type: "json", src: "../data/img/bounce.json" },
-    { name: "controlsTexture", type: "image", src: "../data/img/controlsTexture.png" },
-    { name: "controlsTexture", type: "json", src: "../data/img/controlsTexture.json" },
-    { name: "switch", type: "image", src: "../data/img/switch.png" },
-    { name: "switch", type: "json", src: "../data/img/switch.json" },
-    { name: "machine", type: "image", src: "../data/img/machine.png" },
-    { name: "machine", type: "json", src: "../data/img/machine.json" },
-    { name: "topo", type: "image", src: "../data/img/topo.png" },
-    { name: "topo", type: "json", src: "../data/img/topo.json" },
-    { name: "topo_ground", type: "image", src: "../data/img/topo_ground.png" },
-    { name: "topo_ground", type: "json", src: "../data/img/topo_ground.json" },
-    { name: "egg", type: "image", src: "../data/img/egg.png" },
-    { name: "egg", type: "json", src: "../data/img/egg.json" },
+    { name: "bounce", type: "image", src: "/bounce_tales/data/img/bounce.png" },
+    { name: "bounce", type: "json", src: "/bounce_tales/data/img/bounce.json" },
+    { name: "controlsTexture", type: "image", src: "/bounce_tales/data/img/controlsTexture.png" },
+    { name: "controlsTexture", type: "json", src: "/bounce_tales/data/img/controlsTexture.json" },
+    { name: "switch", type: "image", src: "/bounce_tales/data/img/switch.png" },
+    { name: "switch", type: "json", src: "/bounce_tales/data/img/switch.json" },
+    { name: "machine", type: "image", src: "/bounce_tales/data/img/machine.png" },
+    { name: "machine", type: "json", src: "/bounce_tales/data/img/machine.json" },
+    { name: "topo", type: "image", src: "/bounce_tales/data/img/topo.png" },
+    { name: "topo", type: "json", src: "/bounce_tales/data/img/topo.json" },
+    { name: "topo_ground", type: "image", src: "/bounce_tales/data/img/topo_ground.png" },
+    { name: "topo_ground", type: "json", src: "/bounce_tales/data/img/topo_ground.json" },
+    { name: "egg", type: "image", src: "/bounce_tales/data/img/egg.png" },
+    { name: "egg", type: "json", src: "/bounce_tales/data/img/egg.json" },
 
 
     /* Background music.
     * @example
-    * { name: "example_bgm", type: "audio", src: "../data/bgm/" },
+    * { name: "example_bgm", type: "audio", src: "/bounce_tales/data/bgm/" },
     */
-    { name: "start", type: "audio", src: "../data/bgm/" },
-    { name: "main", type: "audio", src: "../data/bgm/" },
-    { name: "win", type: "audio", src: "../data/bgm/" },
+    { name: "start", type: "audio", src: "/bounce_tales/data/bgm/" },
+    { name: "main", type: "audio", src: "/bounce_tales/data/bgm/" },
+    { name: "win", type: "audio", src: "/bounce_tales/data/bgm/" },
 
 
-    { name: "PressStart2P", type:"image", src: "../data/fnt/PressStart2P.png" },
-    { name: "PressStart2P", type:"binary", src: "../data/fnt/PressStart2P.fnt"}
+    { name: "PressStart2P", type:"image", src: "/bounce_tales/data/fnt/PressStart2P.png" },
+    { name: "PressStart2P", type:"binary", src: "/bounce_tales/data/fnt/PressStart2P.fnt"}
 
 ];
 
